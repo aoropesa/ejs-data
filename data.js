e413f42191dec86c77fab509f31bfa10adb0f6b9
aoropesa <aoropesa@fvi.edu>
2017-11-01 18:29:18 -0400

    add data

--- data.js
+++ data.js
@@ -1,6 +1,65 @@
 const data = [
   {
     "created_at": "Thu Jun 22 21:00:00 +0000 2017",
+    "id": 877994604561387501,
+    "id_str": "877994604561387521",
+    "text":
+      "Creating a Grocery List Manager Using Angular, Part 1: Add &amp; Display Items https://t.co/xFox78juL1 #Angular",
+    "truncated": false,
+    "entities": {
+      "hashtags": [
+        {
+          "text": "Angular",
+          "indices": [103, 111]
+        }
+      ],
+      "symbols": [],
+      "user_mentions": [],
+      "urls": [
+        {
+          "url": "https://t.co/xFox78juL1",
+          "expanded_url": "http://buff.ly/2sr60pf",
+          "display_url": "buff.ly/2sr60pf",
+          "indices": [79, 102]
+        }
+      ]
+    },
+    "source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+    "user": {
+      "id": 772682962,
+      "id_str": "7726829642",
+      "name": "AlexPoint JavaScript",
+      "screen_name": "SitePointJS",
+      "location": "Miami, Estados Unidos",
+      "description": "Keep up with JavaScript tutorials, tips, tricks and articles at SitePoint.",
+      "url": "http://t.co/cCH13gqeUK",
+      "entities": {
+        "url": {
+          "urls": [
+            {
+              "url": "http://t.co/cCH13gqeUK",
+              "expanded_url": "http://sitepoint.com/javascript",
+              "display_url": "sitepoint.com/javascript",
+              "indices": [0, 22]
+            }
+          ]
+        },
+        "description": {
+          "urls": []
+        }
+      },
+      "protected": false,
+      "followers_count": 3245,
+      "friends_count": 18,
+      "listed_count": 328,
+      "created_at": "Sun May 22 02:06:33 +0000 1992",
+      "favourites_count": 57,
+      "utc_offset": 43200,
+      "time_zone": "Kendall"
+    }
+  },
+  {
+    "created_at": "Thu Jun 22 21:00:00 +0000 2017",
     "id": 877994604561387500,
     "id_str": "877994604561387520",
     "text":
